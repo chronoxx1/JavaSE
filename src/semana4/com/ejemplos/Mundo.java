425f16cf1c5c15ee5cfb5cb41bde217502428bcf
José Chávez <jose271_@hotmail.com>
2017-10-16 10:44:39 -0500

    cambio

--- src/semana4/com/ejemplos/Mundo.java
+++ src/semana4/com/ejemplos/Mundo.java
@@ -3,7 +3,10 @@ package semana4.com.ejemplos;
 public class Mundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hola Mundo!");
+			System.out.println("x2");
+		}
 
 	}
 
