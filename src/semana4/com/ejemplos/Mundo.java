581e51768604d4c48ce5be7820cce27c746606d6
José Chávez <jose271_@hotmail.com>
2017-10-16 11:59:23 -0500

    cambio

--- src/semana4/com/ejemplos/Mundo.java
+++ src/semana4/com/ejemplos/Mundo.java
@@ -1,13 +1,24 @@
 package semana4.com.ejemplos;
 
+import java.util.Scanner;
+
 public class Mundo {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Hola Mundo!");
-			System.out.println("x2");
-		}
-
+		
+		Scanner read = new Scanner(System.in);
+		
+		System.out.println("Ingrese..");
+		
+		double a = read.nextDouble();
+		double b = read.nextDouble();
+		double c = read.nextDouble();
+		
+		double x1 = ((-b) + Math.sqrt(Math.pow(b, 2)-(4*a*c))/(2*a));
+		double x2 = ((-b) - Math.sqrt(Math.pow(b, 2)-(4*a*c))/(2*a));
+		
+		System.out.println(x1);
+		System.out.println(x2);
 	}
 
 }
